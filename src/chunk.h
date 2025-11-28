43fcdda03b789e70b955f42554f2f634fd134cc7
ankit <sahuankit.murhu@gmail.com>
2025-11-28 22:34:41 +0530

    feat: bytecode and instruction definations
    feat: bytecode and instruction definations

---
+++ src/chunk.h
@@ -0,0 +1,26 @@
+#ifndef crox_chunk_h
+#define crox_chunk_h
+
+#include "common.h"
+
+/* 
+ * each instruction has one byte operation code(opcode).
+ * it defines what kind of operations we're dealing with.
+*/
+typedef enum{
+  OP_RETURN,
+} OpCode;
+
+/*
+* chunk structure that holds bytecode and other data
+*  - capacity: total allocated size for code array
+*  - count: current number of bytes used in code array
+*  - code: dynamic array of bytes representing bytecode
+*/
+typedef struct{
+  int capacity;
+  int count;
+  uint8_t* code;
+} Chunk;
+
+#endif
