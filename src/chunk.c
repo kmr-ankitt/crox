6fc780f4fad6f9e5d091eaa117c9b66521ad0612
ankit <sahuankit.murhu@gmail.com>
2025-11-28 23:27:17 +0530

    feat: chunk constructor and write byte to chunk

---
+++ src/chunk.c
@@ -0,0 +1,35 @@
+#include "chunk.h"
+#include "memory.h"
+
+/*
+ * initializes a dynamically allocated chunk structure
+ */
+void initChunk(Chunk* chunk){
+  chunk->capacity = 0;
+  chunk->count = 0;
+  chunk->code = NULL;
+}
+
+
+/*
+* writes a byte to chunk's array
+* if current capacity is insufficient
+* - grows the array capacity 
+* else 
+*  simply adds byte to the array
+*/
+void writeChunk(Chunk *chunk, uint8_t byte){
+  if(chunk->capacity < chunk->count + 1) {
+    int oldCapacity = chunk->capacity;
+    chunk->capacity = GROW_CAPACITY(oldCapacity);
+    chunk->code = GROW_ARRAY(
+      uint8_t,
+      chunk->code,
+      oldCapacity,
+      chunk->capacity
+    );
+  }
+
+  chunk->code[chunk->count] = byte;
+  chunk->count++;
+}
--- src/chunk.h
+++ src/chunk.h
@@ -23,4 +23,8 @@ typedef struct{
   uint8_t* code;
 } Chunk;
 
+void initChunk(Chunk* chunk);
+
+void writeChunk(Chunk* chunk, uint8_t byte);
+
 #endif
